71c5cedee497f1538b6701bb9ea769cd980cbad4
Fathismael93 <fathismael@gmail.com>
2025-02-05 21:28:17 -0800

    Added some config files for ESlint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+const eslint = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'next/core-web-vitals',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+  },
+};
+export default eslint;
