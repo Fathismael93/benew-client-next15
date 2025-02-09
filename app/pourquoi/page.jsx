acea30b939e92d72eafaf8c13e3fd35ce8709368
Fathismael93 <fathismael@gmail.com>
2025-02-09 03:41:59 -0800

    Resolved some errors happened

--- app/pourquoi/page.jsx
+++ app/pourquoi/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from './pourquoi.module.scss';
+import './pourquoi.scss';
 
 const PourquoiPage = () => {
   return (
@@ -8,23 +8,23 @@ const PourquoiPage = () => {
       <section className="others">
         <Parallax bgColor="#0c0c1d" title="Présentation" planets="/sun.png" />
       </section>
-      <section className={styles.container}>
-        <nav className={styles.nav}>
-          <Link href="/benew" className={styles.link}>
-            <div className={styles.linkContent}>
-              <span className={styles.linkText}>BENEW</span>
+      <section className="container">
+        <nav className="nav">
+          <Link href="/benew" className="link">
+            <div className="linkContent">
+              <span className="linkText">BENEW</span>
             </div>
           </Link>
 
-          <Link href="/products" className={styles.link}>
-            <div className={styles.linkContent}>
-              <span className={styles.linkText}>Nos Produits</span>
+          <Link href="/products" className="link">
+            <div className="linkContent">
+              <span className="linkText">Nos Produits</span>
             </div>
           </Link>
 
-          <Link href="/team" className={styles.link}>
-            <div className={styles.linkContent}>
-              <span className={styles.linkText}>L'Équipe Benew</span>
+          <Link href="/team" className="link">
+            <div className="linkContent">
+              <span className="linkText">L'Équipe Benew</span>
             </div>
           </Link>
         </nav>
