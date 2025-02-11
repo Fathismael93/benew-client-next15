19a6917d20311a2e7ca5981546f22d8c4c986d19
Fathismael93 <fathismael@gmail.com>
2025-02-11 07:58:15 -0800

    Implemented Porducts Page

--- app/presentation/products/page.jsx
+++ app/presentation/products/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './products.scss';
+import './products-presentation.scss';
 import Parallax from '@/components/parallax';
 
 const ProductsPresentation = () => {
---
+++ app/products/page.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+// pages/portfolio.js
+import Link from 'next/link';
+import Image from 'next/image';
+import './products.scss';
+
+const ProductsPage = () => {
+  const projects = [
+    {
+      id: 1,
+      title: 'Transformation Digitale',
+      image: '/e-commerce.jpg',
+      link: '/projects/digital-transformation',
+    },
+    {
+      id: 2,
+      title: 'Innovation Technologique',
+      image: '/e-commerce2.jpg',
+      link: '/projects/tech-innovation',
+    },
+    {
+      id: 3,
+      title: 'Solutions Cloud',
+      image: '/e-commerce3.jpg',
+      link: '/projects/cloud-solutions',
+    },
+  ];
+
+  return (
+    <div className="portfolioContainer">
+      {projects.map((project) => (
+        <section key={project.id} className="projectSection">
+          <div className="imageContainer">
+            <Image
+              src={project.image}
+              alt={project.title}
+              fill
+              className="projectImage"
+              priority
+            />
+          </div>
+          <Link href={project.link} className="titleLink">
+            <h2 className="projectTitle">{project.title}</h2>
+          </Link>
+        </section>
+      ))}
+    </div>
+  );
+};
+
+export default ProductsPage;
