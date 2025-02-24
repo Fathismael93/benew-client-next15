4cf0eb317d31ca1c3ee49d119d16bdd73fe29609
Fathismael93 <fathismael@gmail.com>
2025-02-24 03:20:02 -0800

    Redesigned the templates list page

--- components/templates/SingleTemplateShops.jsx
+++ components/templates/SingleTemplateShops.jsx
@@ -1,77 +1,53 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
+import Link from 'next/link';
 import { CldImage } from 'next-cloudinary';
-import './styling/templateShops.scss';
+import './styling/templates.scss';
 import Parallax from '@/components/layouts/parallax';
-import OrderModal from '../modal';
+import { MdMonitor, MdPhoneIphone } from 'react-icons/md';
 
-const SingleTemplateShops = ({ applications }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
+const TemplatesList = ({ templates }) => {
   return (
-    <div>
+    <div className="templatesContainer">
       <section className="others">
-        <Parallax
-          bgColor="#0c0c1d"
-          title={
-            applications !== undefined
-              ? applications[0]?.template_name
-              : 'Modèle vide'
-          }
-          planets="/sun.png"
-        />
+        <Parallax bgColor="#0c0c1d" title="Nos Modèles" planets="/sun.png" />
       </section>
-      {applications.map((app) => (
-        <section key={app.application_id} className="others projectSection">
-          <div className="contentWrapper">
+      {templates.map((template) => (
+        <section key={template.template_id} className="others projectSection">
+          <div className="contentCard">
             <div className="imageContainer">
               <CldImage
-                src={app.application_images[0]}
-                alt={app.application_name}
-                width={800}
-                height={1000}
+                src={template.template_image}
+                alt={template.template_name}
+                width={1200}
+                height={800}
                 className="projectImage"
                 priority
               />
-            </div>
-            <div className="detailsContainer">
-              <h4 className="projectTitle">{app.application_name}</h4>
-              <div className="appDetails">
-                <p className="appFee">Prix: {app.application_fee} Fdj</p>
-                <a
-                  href={app.application_link}
-                  className="appLink"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Visiter l'application
-                </a>
-                <div className="buttonGroup">
-                  <button
-                    className="primaryButton"
-                    onClick={() => setIsModalOpen(true)}
-                  >
-                    Commander maintenant
-                  </button>
-                  <a
-                    href={`/templates/${app.application_link}`}
-                    className="secondaryButton"
-                  >
-                    En savoir plus
-                  </a>
-                </div>
+              <div className="platforms">
+                <span className="platformIcon">
+                  <MdMonitor size={24} />
+                </span>
+                {template.template_has_mobile && (
+                  <span className="platformIcon">
+                    <MdPhoneIphone size={24} />
+                  </span>
+                )}
               </div>
             </div>
+            <Link
+              href={`/templates/${template.template_id}`}
+              className="titleLink"
+            >
+              <h4 className="projectTitle">{template.template_name}</h4>
+              <span className="viewDetails">Voir les applications</span>
+            </Link>
           </div>
-          <OrderModal
-            isOpen={isModalOpen}
-            onClose={() => setIsModalOpen(false)}
-          />
         </section>
       ))}
     </div>
   );
 };
 
-export default SingleTemplateShops;
+export default TemplatesList;
