af628ddb159bd3210e7b96cbd44ab20c2cbc44d5
Fathismael93 <fathismael@gmail.com>
2025-06-01 09:20:45 +0300

    Implemented new presentation's first base

--- app/presentation/page.jsx
+++ app/presentation/page.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import './presentation.scss';
 import Parallax from '@/components/layouts/parallax';
 
@@ -10,37 +9,37 @@ function Presentation() {
         <Parallax bgColor="#0c0c1d" title="Présentation" planets="/sun.png" />
       </section>
       <section className="others">
-        <div class="banner">
-          <div class="slider" style="--quantity: 10">
-            <div class="item" style="--position: 1">
-              <img src="images/dragon_1.jpg" alt="" />
+        <div className="banner">
+          <div className="slider" style={{ '--quantity': 10 }}>
+            <div className="item" style={{ '--position': 1 }}>
+              <img src="/images/dragon_1.jpg" alt="" />
             </div>
-            <div class="item" style="--position: 2">
-              <img src="images/dragon_2.jpg" alt="" />
+            <div className="item" style={{ '--position': 2 }}>
+              <img src="/images/dragon_2.jpg" alt="" />
             </div>
-            <div class="item" style="--position: 3">
-              <img src="images/dragon_3.jpg" alt="" />
+            <div className="item" style={{ '--position': 3 }}>
+              <img src="/images/dragon_3.jpg" alt="" />
             </div>
-            <div class="item" style="--position: 4">
-              <img src="images/dragon_4.jpg" alt="" />
+            <div className="item" style={{ '--position': 4 }}>
+              <img src="/images/dragon_4.jpg" alt="" />
             </div>
-            <div class="item" style="--position: 5">
-              <img src="images/dragon_5.jpg" alt="" />
+            <div className="item" style={{ '--position': 5 }}>
+              <img src="/images/dragon_5.jpg" alt="" />
             </div>
-            <div class="item" style="--position: 6">
-              <img src="images/dragon_6.jpg" alt="" />
+            <div className="item" style={{ '--position': 6 }}>
+              <img src="/images/dragon_6.jpg" alt="" />
             </div>
-            <div class="item" style="--position: 7">
-              <img src="images/dragon_7.jpg" alt="" />
+            <div className="item" style={{ '--position': 7 }}>
+              <img src="/images/dragon_7.jpg" alt="" />
             </div>
-            <div class="item" style="--position: 8">
-              <img src="images/dragon_8.jpg" alt="" />
+            <div className="item" style={{ '--position': 8 }}>
+              <img src="/images/dragon_8.jpg" alt="" />
             </div>
-            <div class="item" style="--position: 9">
-              <img src="images/dragon_9.jpg" alt="" />
+            <div className="item" style={{ '--position': 9 }}>
+              <img src="/images/dragon_9.jpg" alt="" />
             </div>
-            <div class="item" style="--position: 10">
-              <img src="images/dragon_10.jpg" alt="" />
+            <div className="item" style={{ '--position': 10 }}>
+              <img src="/images/dragon_10.jpg" alt="" />
             </div>
           </div>
         </div>
