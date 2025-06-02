25622d3f2d52c9f3f3224757aa34d5151feb85c5
Fathismael93 <fathismael@gmail.com>
2025-06-02 10:19:40 +0300

    Added presentations image of dragon ball universe

--- app/presentation/page.jsx
+++ app/presentation/page.jsx
@@ -17,36 +17,15 @@ function Presentation() {
         />
         <div className="stars" />
         <div className="banner">
-          <div className="slider" style={{ '--quantity': 10 }}>
+          <div className="slider" style={{ '--quantity': 3 }}>
             <div className="item" style={{ '--position': 1 }}>
-              <img src="/images/dragon_1.jpg" alt="" />
+              <img src="/images/the_announcer.png" alt="" />
             </div>
             <div className="item" style={{ '--position': 2 }}>
-              <img src="/images/dragon_2.jpg" alt="" />
+              <img src="/images/the_product.png" alt="" />
             </div>
             <div className="item" style={{ '--position': 3 }}>
-              <img src="/images/dragon_3.jpg" alt="" />
-            </div>
-            <div className="item" style={{ '--position': 4 }}>
-              <img src="/images/dragon_4.jpg" alt="" />
-            </div>
-            <div className="item" style={{ '--position': 5 }}>
-              <img src="/images/dragon_5.jpg" alt="" />
-            </div>
-            <div className="item" style={{ '--position': 6 }}>
-              <img src="/images/dragon_6.jpg" alt="" />
-            </div>
-            <div className="item" style={{ '--position': 7 }}>
-              <img src="/images/dragon_7.jpg" alt="" />
-            </div>
-            <div className="item" style={{ '--position': 8 }}>
-              <img src="/images/dragon_8.jpg" alt="" />
-            </div>
-            <div className="item" style={{ '--position': 9 }}>
-              <img src="/images/dragon_9.jpg" alt="" />
-            </div>
-            <div className="item" style={{ '--position': 10 }}>
-              <img src="/images/dragon_10.jpg" alt="" />
+              <img src="/images/maitre_kaio.png" alt="" />
             </div>
           </div>
         </div>
