e22ec746891c39ecb5d524694234073e8f734606
Fathismael93 <fathismael@gmail.com>
2025-06-22 12:48:30 +0300

    Checking where the problem of the build is coming from for the blog page

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -4,22 +4,22 @@ import articleIDSchema from '@/utils/schema';
 import SinglePost from '@/components/blog/SinglePost';
 
 async function getSinglePost(id) {
-  let article;
-  try {
-    await articleIDSchema.validate({ id });
+  let article = {};
+  // try {
+  //   await articleIDSchema.validate({ id });
 
-    await axios
-      .get(`https://benew-client-next15.vercel.app/api/blog/${id}`)
-      .then((response) => {
-        console.log('Response from API:', response.data);
-        article = response.data.data || {};
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  } catch (error) {
-    console.log(error);
-  }
+  //   await axios
+  //     .get(`https://benew-client-next15.vercel.app/api/blog/${id}`)
+  //     .then((response) => {
+  //       console.log('Response from API:', response.data);
+  //       article = response.data.data || {};
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // } catch (error) {
+  //   console.log(error);
+  // }
 
   return article;
 }
