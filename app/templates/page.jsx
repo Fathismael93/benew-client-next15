4bea53d8030b47738ec3244a99fc35997223b2ec
Fathismael93 <fathismael@gmail.com>
2025-06-22 13:57:37 +0300

    Checking where the problem of the build is coming from

--- app/templates/page.jsx
+++ app/templates/page.jsx
@@ -6,7 +6,7 @@ async function getTemplates() {
   const client = await getClient();
   try {
     const result = await client.query(
-      'SELECT template_id, template_name, template_image, template_has_web, template_has_mobile FROM catalog.templates ORDER BY template_added DESC AND is_active = t',
+      'SELECT template_id, template_name, template_image, template_has_web, template_has_mobile FROM catalog.templates WHERE is_active=true ORDER BY template_added DESC',
     );
     return result.rows;
   } catch (error) {
