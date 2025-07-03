a999d38b047fc1898c66f2ec3bfb2bfdf1b32fd1
Fathismael93 <fathismael@gmail.com>
2025-07-03 09:07:33 +0300

    Created server action for adding order

--- components/modal/OrderModal.jsx
+++ components/modal/OrderModal.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { createOrder } from './actions/orderActions';
+import { createOrder } from '../../actions/orderActions';
 import './orderModal.scss';
 
 const OrderModal = ({
