14b4d87f09791fdff26f856c625943be1d1245f2
Fathismael93 <fathismael@gmail.com>
2025-07-09 22:19:07 +0300

    Updated sidebar

--- components/layouts/sidebar/index.jsx
+++ components/layouts/sidebar/index.jsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect, useCallback } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { useRouter } from 'next/navigation';
 import Links from './links';
 import ToggleButton from './toggleButton';
 import './sidebar.scss';
