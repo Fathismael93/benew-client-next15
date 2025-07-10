88f08a53c99682b33c24fd90e65c0cfc3e4192f7
Fathismael93 <fathismael@gmail.com>
2025-07-10 19:06:42 +0300

    Updated TemplatesList.jsx

--- components/templates/TemplatesList.jsx
+++ components/templates/TemplatesList.jsx
@@ -2,11 +2,14 @@
 
 import Link from 'next/link';
 import { CldImage } from 'next-cloudinary';
+import { useState } from 'react';
 import './styling/templates.scss';
 import Parallax from '@/components/layouts/parallax';
 import { MdMonitor, MdPhoneIphone } from 'react-icons/md';
 
 const TemplatesList = ({ templates }) => {
+  const [hoveredCard, setHoveredCard] = useState(null);
+
   return (
     <div className="templatesContainer">
       <section className="others">
@@ -15,35 +18,59 @@ const TemplatesList = ({ templates }) => {
       {templates.length !== undefined &&
         templates.map((template) => (
           <section key={template.template_id} className="others projectSection">
-            <div className="contentCard">
-              <div className="imageContainer">
-                <CldImage
-                  src={template.template_image}
-                  alt={template.template_name}
-                  width={1200}
-                  height={800}
-                  className="projectImage"
-                  priority
-                />
-                <div className="platforms">
-                  <span className="platformIcon">
-                    <MdMonitor size={24} />
-                  </span>
-                  {template.template_has_mobile && (
-                    <span className="platformIcon">
-                      <MdPhoneIphone size={24} />
-                    </span>
-                  )}
+            <Link
+              href={`/templates/${template.template_id}`}
+              className="templateCard"
+              style={{
+                background:
+                  template.template_color ||
+                  'linear-gradient(135deg, #667eea 0%, #764ba2 100%)',
+              }}
+              onMouseEnter={() => setHoveredCard(template.template_id)}
+              onMouseLeave={() => setHoveredCard(null)}
+            >
+              <div
+                className={`templateCardInner ${hoveredCard === template.template_id ? 'hovered' : ''}`}
+              >
+                {/* Conteneur image (70% de la hauteur) */}
+                <div className="templateImageContainer">
+                  <div className="templateAppImage">
+                    <CldImage
+                      src={template.template_image}
+                      alt={`Interface de ${template.template_name}`}
+                      width={1200}
+                      height={800}
+                      className="templateImage"
+                      priority
+                    />
+                  </div>
+
+                  {/* Gradients overlay */}
+                  <div className="gradientOverlay"></div>
+                  <div className="gradientLeft"></div>
+                  <div className="gradientRight"></div>
+                </div>
+
+                {/* Conteneur info (30% de la hauteur) */}
+                <div className="templateInfoContainer">
+                  <div className="templateAppInfo">
+                    <h3 className="templateName">{template.template_name}</h3>
+                    <div className="templatePlatforms">
+                      {template.template_has_web && (
+                        <span className="templatePlatformIcon">
+                          <MdMonitor size={20} />
+                        </span>
+                      )}
+                      {template.template_has_mobile && (
+                        <span className="templatePlatformIcon">
+                          <MdPhoneIphone size={20} />
+                        </span>
+                      )}
+                    </div>
+                  </div>
                 </div>
               </div>
-              <Link
-                href={`/templates/${template.template_id}`}
-                className="titleLink"
-              >
-                <h4 className="projectTitle">{template.template_name}</h4>
-                <span className="viewDetails">Voir les applications</span>
-              </Link>
-            </div>
+            </Link>
           </section>
         ))}
     </div>
