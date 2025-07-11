18c78af102be7d06c4aa92f9b6072d7f51186a62
Fathismael93 <fathismael@gmail.com>
2025-07-11 20:45:16 +0300

    Updated SingleTemplateShops.jsx

--- app/templates/[id]/page.jsx
+++ app/templates/[id]/page.jsx
@@ -150,7 +150,7 @@ function getTemplateDataQuery(templateId) {
           a.application_images[0], 
           a.application_level
           t.template_id, 
-          t.template_name,
+          t.template_name
         FROM catalog.applications a
         JOIN catalog.templates t ON a.application_template_id = t.template_id 
         WHERE a.application_template_id = $1 
