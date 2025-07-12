70d512c0ddb7f26962f7ad57ee9c5e17221c08a3
Fathismael93 <fathismael@gmail.com>
2025-07-12 15:57:52 +0300

    Checking something with SingleTemplateShops.jsx

--- components/templates/SingleTemplateShops.jsx
+++ components/templates/SingleTemplateShops.jsx
@@ -74,88 +74,107 @@ const SingleTemplateShops = ({ templateID, applications, platforms }) => {
           planets="/sun.png"
         />
       </section>
-
-      {applications.length !== undefined &&
-        applications.map((app) => (
-          <section key={app.application_id} className="others projectSection">
-            <div className="application-card">
-              <div className="card-image">
-                <CldImage
-                  src={app.application_images[0]}
-                  alt={app.application_name}
-                  width={400}
-                  height={200}
-                  className="app-image"
-                  priority
-                />
-                {/* <div className="image-overlay">
-                  <div className="stats-badge">
-                    <span className="level-badge">
-                      Niveau {app.application_level}
-                    </span>
-                  </div>
-                </div> */}
-              </div>
-
-              <div className="card-content">
-                <h3 className="app-title">
-                  {app.application_name} | {app.application_category}
-                </h3>
-                <p className="app-subtitle">
-                  Type:{' '}
-                  <span className="level">
-                    {getApplicationLevelLabel(app.application_level)}
-                  </span>
-                </p>
-
-                <div className="card-footer">
-                  <div className="price-section">
-                    <span className="price">
-                      {formatPrice(app.application_fee)} FDJ
-                    </span>
-                    <div className="rent">
-                      <span className="rent-price">
-                        {formatPrice(app.application_rent)} FDJ/mois
-                      </span>
-                    </div>
-                  </div>
-
-                  <div className="action-buttons">
-                    <button
-                      className="btn btn-cart"
-                      onClick={() => handleOrderClick(app)}
-                      disabled={!platforms || platforms.length === 0}
-                    >
-                      <FaDollarSign size={16} />
-                    </button>
-                    <Link
-                      href={`/templates/${templateID}/applications/${app.application_id}`}
-                      className="btn btn-preview"
-                    >
-                      <IoEye size={16} />
-                      Voir +
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-        ))}
-
-      {selectedApp && (
-        <OrderModal
-          isOpen={isModalOpen}
-          onClose={() => {
-            setIsModalOpen(false);
-            setSelectedApp(null);
-          }}
-          platforms={platforms}
-          applicationId={selectedApp.application_id}
-          applicationFee={selectedApp.application_fee}
-        />
-      )}
+      <section className="others projectSection">
+        <h1>Pour les boutiques du templaye {applications[0]?.template_name}</h1>
+      </section>
     </div>
   );
+
+  // return (
+  //   <div className="appsContainer">
+  //     <section className="others">
+  //       <Parallax
+  //         bgColor="#0c0c1d"
+  //         title={
+  //           applications !== undefined
+  //             ? applications[0]?.template_name
+  //             : 'Modèle vide'
+  //         }
+  //         planets="/sun.png"
+  //       />
+  //     </section>
+
+  //     {applications.length !== undefined &&
+  //       applications.map((app) => (
+  //         <section key={app.application_id} className="others projectSection">
+  //           <div className="application-card">
+  //             <div className="card-image">
+  //               <CldImage
+  //                 src={app.application_images[0]}
+  //                 alt={app.application_name}
+  //                 width={400}
+  //                 height={200}
+  //                 className="app-image"
+  //                 priority
+  //               />
+  //               {/* <div className="image-overlay">
+  //                 <div className="stats-badge">
+  //                   <span className="level-badge">
+  //                     Niveau {app.application_level}
+  //                   </span>
+  //                 </div>
+  //               </div> */}
+  //             </div>
+
+  //             <div className="card-content">
+  //               <h3 className="app-title">
+  //                 {app.application_name} | {app.application_category}
+  //               </h3>
+  //               <p className="app-subtitle">
+  //                 Type:{' '}
+  //                 <span className="level">
+  //                   {getApplicationLevelLabel(app.application_level)}
+  //                 </span>
+  //               </p>
+
+  //               <div className="card-footer">
+  //                 <div className="price-section">
+  //                   <span className="price">
+  //                     {formatPrice(app.application_fee)} FDJ
+  //                   </span>
+  //                   <div className="rent">
+  //                     <span className="rent-price">
+  //                       {formatPrice(app.application_rent)} FDJ/mois
+  //                     </span>
+  //                   </div>
+  //                 </div>
+
+  //                 <div className="action-buttons">
+  //                   <button
+  //                     className="btn btn-cart"
+  //                     onClick={() => handleOrderClick(app)}
+  //                     disabled={!platforms || platforms.length === 0}
+  //                   >
+  //                     <FaDollarSign size={16} />
+  //                   </button>
+  //                   <Link
+  //                     href={`/templates/${templateID}/applications/${app.application_id}`}
+  //                     className="btn btn-preview"
+  //                   >
+  //                     <IoEye size={16} />
+  //                     Voir +
+  //                   </Link>
+  //                 </div>
+  //               </div>
+  //             </div>
+  //           </div>
+  //         </section>
+  //       ))}
+
+  //     {selectedApp && (
+  //       <OrderModal
+  //         isOpen={isModalOpen}
+  //         onClose={() => {
+  //           setIsModalOpen(false);
+  //           setSelectedApp(null);
+  //         }}
+  //         platforms={platforms}
+  //         applicationId={selectedApp.application_id}
+  //         applicationFee={selectedApp.application_fee}
+  //       />
+  //     )}
+  //   </div>
+  // );
 };
 
 export default SingleTemplateShops;
