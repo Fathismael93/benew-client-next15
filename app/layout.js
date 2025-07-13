3f3bcc2059487bd07d1a2de48c0b3de7c9d571c8
Fathismael93 <fathismael@gmail.com>
2025-07-13 10:05:46 +0300

    Reorganized scss files

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
-import './app.scss';
+// app/layout.js
+import './styles/main.scss'; // ✅ Nouvelle référence
 import Navbar from '../components/layouts/navbar';
 
 export default function RootLayout({ children }) {
