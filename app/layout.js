f4726a502ff4622348d9cdbf92547514dee7a206
Fathismael93 <fathismael@gmail.com>
2025-07-23 16:02:35 +0300

    Implemented google analytics

--- app/layout.js
+++ app/layout.js
@@ -127,15 +127,6 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body>
-        {/* GTM noscript */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K4F2X42H"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
         <Navbar />
         {children}
 
@@ -154,6 +145,25 @@ export default function RootLayout({ children }) {
             <AnalyticsInitializer isDevelopment />
           </>
         )}
+
+        {/* Partie script de GTM */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K4F2X42H');
+            `,
+          }}
+        />
       </body>
     </html>
   );
