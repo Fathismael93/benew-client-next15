2342bd537d5be2ab72746fca5029d68f75dccec0
Fathismael93 <fathismael@gmail.com>
2025-08-03 11:46:08 +0300

    Updated qualities.scss

--- components/layouts/home/qualities/index.jsx
+++ components/layouts/home/qualities/index.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import './qualities.scss';
+import './index.scss';
 import { useEffect, useState } from 'react';
 import { MdPalette, MdPayment, MdSecurity, MdVerified } from 'react-icons/md';
 
