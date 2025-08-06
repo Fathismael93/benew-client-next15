4e5f0c5552dfff895e383c9cf73f721954fda6fe
Fathismael93 <fathismael@gmail.com>
2025-08-06 14:04:31 +0300

    Updated layout.js

--- app/layout.js
+++ app/layout.js
@@ -112,24 +112,6 @@ export default function RootLayout({ children }) {
         <meta name="theme-color" content="#f6a037" />
         <meta name="application-name" content="Benew" />
         <meta name="msapplication-TileColor" content="#f6a037" />
-
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-K4F2X42H');
-            `,
-          }}
-        />
       </head>
       <body>
         <Navbar />
