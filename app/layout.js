04337b0d2f0e8292f9ee642c855400224a7b4201
Fathismael93 <fathismael@gmail.com>
2025-08-14 21:51:30 +0300

    Updated TemplatesList.jsx

--- app/layout.js
+++ app/layout.js
@@ -114,11 +114,17 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         {/* GTM EN PREMIER - CRITIQUE */}
-        {gtmId && <GoogleTagManager gtmId={gtmId} />}
-
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-K4F2X42H');`,
+          }}
+        />
         <Navbar />
         {children}
-
         {/* AnalyticsInitializer peut rester après */}
         {gtmId && (
           <AnalyticsInitializer
