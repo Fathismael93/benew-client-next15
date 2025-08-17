b7252d99a65f7712cb7b81c3b35b431603634ac4
Fathismael93 <fathismael@gmail.com>
2025-08-17 17:51:33 +0300

    Handling issue in contact page

--- components/layouts/home/marketing/index.jsx
+++ components/layouts/home/marketing/index.jsx
@@ -2,9 +2,13 @@
 
 import './index.scss';
 import Image from 'next/image';
-import Link from 'next/link';
+// import Link from 'next/link';
 
 const MarketingHome = () => {
+  const handleBlogClick = () => {
+    window.location.href = '/blog';
+  };
+
   return (
     <div className="main-content">
       <Image
@@ -21,9 +25,9 @@ const MarketingHome = () => {
         <h2 className="main-title">PAIES MOINS DE CHARGES</h2>
 
         {/* AJOUTER CE BOUTON ICI */}
-        <Link href="/blog" className="profit-blog-link">
+        <button onClick={handleBlogClick} className="profit-blog-link">
           En savoir plus
-        </Link>
+        </button>
       </div>
     </div>
   );
